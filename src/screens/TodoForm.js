dad87fb49d078cc00d78776c6c3677d2dd424f4c
sushmita <mmsushma9999@gmail.com>
2020-12-22 10:07:59 +0530

    add list

--- src/screens/TodoForm.js
+++ src/screens/TodoForm.js
@@ -1,27 +1,79 @@
-import React,{useState} from 'react';
+import React from "react";
 import Toolbar from '../components/Toolbar/Toolbar';
-import './screens.css';
-function TodoForm(props){
-    
-    const[input,setInput] = useState('');
 
-    const handleChange = e => {
-        setInput(e.target.value);
-    }
+export default function TodoForm() {
+  const [todo, setTodo] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-    };
-    
-    return(
-        <div>
-            <Toolbar/>
-            <form className='todo-form' onSubmit={handleSubmit}>
-                <input type='text' placeholder="add ToDo" className="todo-input" value={input}  name='text' onChange={handleChange}/>
-                <button className="add-todo">add-todo</button>
-            </form>
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
 
-        </div>
-    )
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        text: todo,
+        completed: false
+      }
+    ]);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (todo === "") return;
+    addTodo();
+    setTodo("");
+  };
+
+  const removeTodo = todoId => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  const toggleTodo = todoId => {
+    const updatedTodos = todos.map(todo => {
+      return todo.id === todoId
+        ? { ...todo, completed: !todo.completed }
+        : todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <label htmlFor="todo">Todo text</label>
+        <br />
+        <input
+          id="todo"
+          className="todo-input"
+          onChange={handleChange}
+          value={todo}
+        />
+        <button type="submit" className="add-btn">
+          Add
+        </button>
+      </form>
+      <div>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <span
+                className={todo.completed ? "todo-completed" : undefined}
+                onClick={() => toggleTodo(todo.id)}
+              >
+                {todo.text}
+              </span>
+              <span className="delete-btn" onClick={() => removeTodo(todo.id)}>
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  );
 }
-export default TodoForm;
